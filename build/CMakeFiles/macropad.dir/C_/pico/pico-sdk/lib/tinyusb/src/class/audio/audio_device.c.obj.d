b44a39c60605cb92a1f06bf6cbb9ce0c9871ea26
marianoirato <marianoirato@hotmail.com>
2022-10-13 20:34:43 -0300

    initial

---
+++ build/CMakeFiles/macropad.dir/C_/pico/pico-sdk/lib/tinyusb/src/class/audio/audio_device.c.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles\macropad.dir\C_\pico\pico-sdk\lib\tinyusb\src\class\audio\audio_device.c.obj: \
+ C:\pico\pico-sdk\lib\tinyusb\src\class\audio\audio_device.c \
+ C:\pico\pico-sdk\lib\tinyusb\src/tusb_option.h \
+ C:\pico\projects\macropad/tusb_config.h
